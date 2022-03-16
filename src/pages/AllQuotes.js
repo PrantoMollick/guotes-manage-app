94368456db658cd3565e9602a285183ed4c597bc
== <prantocse01@gmail.com>
2022-03-17 01:08:50 +0600

    adding dummy data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,15 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+
+const DUMMY_QUOTES = [
+{ id: 'q1', author: 'Pranto', text: 'Learn React router with fun'}, 
+{ id: 'q2', author: 'Mollick', text: 'React is great ui librery'}, 
+
+];
+
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
+  return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuotes;
