b4e507fb637e5f0e0e1f54fb999183764cf5522a
== <prantocse01@gmail.com>
2022-03-17 12:46:14 +0600

    implement pogramatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const [isAddQuote, setIsAddQuote] = useState(false);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!isAddQuote) {
+      return;
+    }
+    navigate('/quotes')
+  }, [isAddQuote])
+
   const addQuoteHandler = (quoteData) => {
       console.log(quoteData);
+      setIsAddQuote(true);
   }
   
 
