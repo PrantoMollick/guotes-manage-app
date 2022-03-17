09f7817d58a7986d6a2db3d7079e33a40e952350
== <prantocse01@gmail.com>
2022-03-17 20:56:01 +0600

    add lazyload to optimize this project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React, { Suspense} from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
+
 import Comments from './components/comments/Comments';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'))
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+
 
 function App() {
   return (
     <Layout>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
       <Routes>
         <Route path='/' element={ <Navigate to='quotes' /> } />
         <Route path='quotes' element={ <AllQuotes /> } />
@@ -19,6 +27,7 @@ function App() {
         <Route path='/new-quote' element={<NewQuote />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      </Suspense>
     </Layout>
   );
 }
