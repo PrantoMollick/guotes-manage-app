9b5aee08cbc3c41166624fdd5f69acdcfd3a515f
== <prantocse01@gmail.com>
2022-03-17 12:32:13 +0600

    no found fallback add 404

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,29 @@
 import { Fragment } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Pranto', text: 'Learn React router with fun'}, 
+  { id: 'q2', author: 'Mollick', text: 'React is great ui librery'}, 
+  
+];
 
 const QuoteDetail = () => {
   const params = useParams();
-  console.log(params);
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
       <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
+
       <Outlet />
+
     </Fragment>
   );
 };
